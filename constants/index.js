f678b0e698a3c2bd16283a44b96cb39689a35974
N918 <136599755+N91874@users.noreply.github.com>
2023-07-23 12:37:08 +0300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xb0bb93a767c580a7505d790b62e8e4534b586a3c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
